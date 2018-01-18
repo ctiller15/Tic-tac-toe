2b9aa0acdc73f5df58971d9f8f3ae4abac31f963
ctiller15 <christiller2015@gmail.com>
2018-01-18 11:01:51 -0500

    add logic to modify board upon player clicks

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,21 +1,48 @@
 import React from 'react';
 
-function squares() {
-	var arr = new Array(9).fill("");
-	return arr.map((date, i) => {
-		return 	(
-			<div key={i + 1} className={`square square${i + 1}`}></div>
-		);
-	});
-}
+var human = 'X';
+var ai = 'O';
 
-function GameBoard() {
-	return(
-		<div className="gameboard">
-			{squares()}
-		</div>
-	);
+class GameBoard extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			boardVals: ["","","","","","","","",""],
+		}
+	}
 
+	handleClick(index) {
+		console.log(`Clicked square ${index}!`);
+		let newArr = this.state.boardVals;
+		if(newArr[index] === "") {
+			newArr[index] = 'X';
+			console.log(newArr);
+			this.setState({boardVals: newArr});			
+		} else {
+			console.log("That one has already been clicked!");
+		}
+
+	}
+
+	squares() {
+		var arr = new Array(9).fill("");
+		return arr.map((date, i) => {
+			return 	(
+				<div key={i + 1} className={`square square${i + 1}`
+				} onClick={() => { this.handleClick(i) }}>
+					{this.state.boardVals[i]}
+				</div>
+			);
+		});
+	}
+
+	render() {
+		return(
+			<div className="gameboard">
+				{this.squares()}
+			</div>
+		);		
+	}
 }
 
 export default GameBoard;
